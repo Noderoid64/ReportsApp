cb1b950067e4ab2171ae0dcc41aab1f6e9eadcc5
noderoid64 <noderoid64@gmail.com>
2021-02-24 10:38:05 +0200

    (refactoring) remove report-page

--- client/src/app/feature/reports/reports-routing.module.ts
+++ client/src/app/feature/reports/reports-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ReportPageComponent } from './pages/report-page/report-page.component';
 import { TaskPageComponent } from './pages/task-page/task-page.component';
 
 export const routes: Routes = [
-    {
-        path: '', component: ReportPageComponent
-    },
     {
         path: 'tasks', component: TaskPageComponent,
     },
--- client/src/app/feature/reports/reports.module.ts
+++ client/src/app/feature/reports/reports.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskPageComponent } from './pages/task-page/task-page.component';
-import { ReportPageComponent } from './pages/report-page/report-page.component';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { TaskTableComponent } from './pages/task-page/task-table/task-table.component';
@@ -12,7 +11,6 @@ import { TaskAddDialogComponent } from './pages/task-page/task-add-dialog/task-a
 @NgModule({
   declarations: [
     TaskPageComponent,
-    ReportPageComponent,
     TaskTableComponent,
     TaskAddDialogComponent
   ],
